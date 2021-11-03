0b58d2569feb6654bbcab7aed45722d08670a559
Vitor Martins <vitorhpmartins@icloud.com>
2021-11-03 18:57:04 -0300

    Lógica de Programação e Projeto

---
+++ Minicurso-Hashtag/Aula03-Logica-de-Programacao-e-Projeto.py
@@ -0,0 +1,11 @@
+# importar a base de dados
+
+# visualizar a base de dados
+
+# faturamento por loja
+
+# quantidade de produtos vendidos por loja
+
+# ticket médio por produto em cada loja
+
+# enviar um e-amil com o relatório
